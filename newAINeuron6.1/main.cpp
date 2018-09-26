4af0017c3d9db86de42ada29d632d5fd0b85bc06
Eric <ericjang@kimo.com>
2018-09-26 19:10:24 +0800

    Initial Commit

---
+++ newAINeuron6.1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  newAINeuron6.1
+//
+//  Created by Eric Cheng on 9/26/18.
+//  Copyright © 2018 Eric Cheng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
